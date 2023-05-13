92324354332c96b673844343002ebeabc3143b73
Gitalexzhong <66319418+Gitalexzhong@users.noreply.github.com>
2023-05-13 17:53:35 +1000

    added string manipulation

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
+
+
+
+
+
 fn main() {
+    let line1 = " ██████ ██████      ██████ ██████";
+    let mut line2: String = String::from(" ");
+    line2 += "    ██     ██  ██      ██     ██";
+    
     println!("");
 
-    // loop {
-    println!("     ██ ██████      ██████ ██████");
-    println!("     ██     ██  ██      ██     ██");
+    println!("{}", line1);
+    println!("{}", line2);
     println!("     ██     ██      ██████ ██████");
     println!("     ██     ██  ██  ██         ██");
     println!("     ██     ██      ██████ ██████");
-    // }
 
     println!("");
 
