6fee854814b07a46486d7c3c9acd01195650706c
Gitalexzhong <66319418+Gitalexzhong@users.noreply.github.com>
2023-05-14 22:57:51 +1000

    Added basic digit 1 printing

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,36 @@
 use grux::GridWriter;
 
-fn main() {
+fn add_big_number(display: &mut String, start: u32, digit: u32) {
+    match digit {
+        1 => {
+            for row in start+4..start+5 {
+                for col in 1..5 {
+                    display.set((row as usize, col), '█');
+                }
+            }
+        },
+        _ => todo!()
+    }
 
-    let mut term_clock = String::new();
-    term_clock.set((1, 2), '█');
-
-    display(&term_clock);
-    display(&term_clock);
 }
 
 fn display(term_clock: &String) {
     print!("{}[2J", 27 as char);
+    // print!("{esc}[2J{esc}[0;0H", esc = 27 as char);
+    // print!("\x1B[2J");
+
     println!("{}", term_clock);
     println!("");
+}
+
+fn main() {
+    let mut term_clock = String::new();
+    term_clock.set((80,24), ' ');
+
+    add_big_number(&mut term_clock, 1, 1);
+
+    display(&term_clock);
+    add_big_number(&mut term_clock, 8, 1);
+
+    display(&term_clock);
 }
