a41383e05eee6f30f8e44d436b971f8b9e045f24
Gitalexzhong <66319418+Gitalexzhong@users.noreply.github.com>
2023-05-14 22:31:49 +1000

    Added basic display feature

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,14 @@
-
-
-
-
+use grux::GridWriter;
 
 fn main() {
-    let line1 = " ██████ ██████      ██████ ██████";
-    let mut line2: String = String::from(" ");
-    line2 += "    ██     ██  ██      ██     ██";
-    
+
+    let mut term_clock = String::new();
+    term_clock.set((1, 2), '█');
+
+    display(&term_clock);
+}
+
+fn display(term_clock: &String) {
+    println!("{}", term_clock);
     println!("");
-
-    println!("{}", line1);
-    println!("{}", line2);
-    println!("     ██     ██      ██████ ██████");
-    println!("     ██     ██  ██  ██         ██");
-    println!("     ██     ██      ██████ ██████");
-
-    println!("");
-
 }
