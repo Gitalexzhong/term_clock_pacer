0ed979794cf3ef20cdcb53cac3fa2cf1bf0bf060
Gitalexzhong <66319418+Gitalexzhong@users.noreply.github.com>
2023-05-14 22:33:48 +1000

    Modified to remove previous terminal

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,11 @@ fn main() {
     term_clock.set((1, 2), '█');
 
     display(&term_clock);
+    display(&term_clock);
 }
 
 fn display(term_clock: &String) {
+    print!("{}[2J", 27 as char);
     println!("{}", term_clock);
     println!("");
 }
