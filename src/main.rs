73cdc26a6998ec5b49aea0472c3487ff3177bc5c
Gitalexzhong <66319418+Gitalexzhong@users.noreply.github.com>
2023-05-23 22:01:29 +1000

    Added clock time layout

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::{ thread::sleep, time::Duration };
-use chrono::{ Local, Timelike, DateTime };
+use chrono::{ Local, Timelike, DateTime, Duration as ChDuration };
 use color_char::Character;
 
 enum Line {
@@ -194,13 +194,26 @@ fn update_time(term_clock: &mut [[Character; 80]; 24]) {
 }
 
 fn update_exam_time(term_clock: &mut [[Character; 80]; 24], exam: &mut exam_status) {
-    
+    let exam_time_color = 32;
+
+    // Display start time
+    let mut start_time = exam.start.hour().to_string() + ":" + &exam.start.minute().to_string();
+    for i in 0..start_time.len() {
+        term_clock[9][i + 1] = Character::new(start_time.remove(0), exam_time_color);
+    }
+
+    // Display end time
+    let end_time = exam.start + ChDuration::hours(exam.duration_hour) + ChDuration::minutes(exam.duration_min);
+    let mut end_time = end_time.hour().to_string() + ":" + &end_time.minute().to_string();
+    for i in 0..end_time.len() {
+        term_clock[9][32 - i] = Character::new(end_time.pop().unwrap(), exam_time_color);
+    }
 }
 
 struct exam_status {
     start: DateTime<Local>,
-    duration_hour: u32,
-    duration_min: u32,
+    duration_hour: i64,
+    duration_min: i64,
 }
 
 fn main() {
