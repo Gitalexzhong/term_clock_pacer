00c56b800b23a0108dbe14d8312a42eee41362df
Gitalexzhong <66319418+Gitalexzhong@users.noreply.github.com>
2023-05-24 22:52:49 +1000

    Addd time working out

--- src/exam_timer.rs
+++ src/exam_timer.rs
@@ -7,23 +7,36 @@ pub(crate) struct ExamStatus {
     pub(crate) duration_min: i64,
 }
 
-fn add_start_end_duration(term_clock: &mut [[Character; 80]; 24], exam: &mut ExamStatus, color: u32) {
+fn add_start_end_duration(
+    term_clock: &mut [[Character; 80]; 24],
+    exam: &mut ExamStatus,
+    color: u32
+) {
     // Calculate start time
     let start_time = exam.start.hour().to_string() + ":" + &exam.start.minute().to_string();
 
     // Calculate end time
-    let end_time = exam.start + Duration::hours(exam.duration_hour) + Duration::minutes(exam.duration_min);
-    let end_time = end_time.hour().to_string() + ":" + &end_time.minute().to_string() + ":" + &end_time.second().to_string();
-    
+    let end_time =
+        exam.start + Duration::hours(exam.duration_hour) + Duration::minutes(exam.duration_min);
+    let end_time_str =
+        end_time.hour().to_string() +
+        ":" +
+        &end_time.minute().to_string() +
+        ":" +
+        &end_time.second().to_string();
+
     // Combined statement
-    let mut duration_time = start_time + " - " +  &end_time;
+    let mut duration_time = start_time + " - " + &end_time_str;
     for i in 0..duration_time.len() {
         term_clock[9][i + 1] = Character::new(duration_time.remove(0), color);
     }
 
+    let a = exam.start;
+    
     // for i in 0..end_time.len() {
     //     term_clock[9][32 - i] = Character::new(end_time.pop().unwrap(), color);
     // }
+
 }
 
 pub(crate) fn update_exam_time(term_clock: &mut [[Character; 80]; 24], exam: &mut ExamStatus) {
