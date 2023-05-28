d1ab4089b9aefd30b69430bf6a9b4e0afd7aee8b
Gitalexzhong <66319418+Gitalexzhong@users.noreply.github.com>
2023-05-28 19:17:25 +1000

    Reduced flickering

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::{ thread::sleep, time::Duration, io::Stdout };
-use chrono::{ Local, Timelike };
+use chrono::{ Local };
 use color_char::Character;
 
 mod clock;
@@ -8,27 +8,28 @@ use clock::update_time;
 mod exam_timer;
 use exam_timer::{ ExamStatus, update_exam_time };
 
-use std::io::{stdout, Write};
-use crossterm::{
-    cursor, terminal, Result, ExecutableCommand
-};
+use std::io::{ stdout, Write };
+use crossterm::{ cursor, terminal, Result, ExecutableCommand };
 
 fn display(term_clock: &[[Character; 80]; 24], stdout: &mut Stdout) -> Result<()> {
-    // print!("{}[2J", 27 as char);
-    // for _ in 0..24 {
-    //     print!("\r");
-
-    // }
-
-    stdout.execute(cursor::MoveUp(24))?;
-    stdout.execute(terminal::Clear(terminal::ClearType::FromCursorDown))?;
-
-    for i in 0..24 {
-        for j in 0..80 {
-            write!(stdout, "\x1b[0;{}m{}\x1b[0m", term_clock[i][j].get_color(), term_clock[i][j])?;
+    let mut stdout_all = "".to_string();
+    // for i in 0..24 {
+    for i in 0..12 {
+        for j in 0..40 {
+        // for j in 0..80 {
+            // write!(stdout, "\x1b[0;{}m{}\x1b[0m", term_clock[i][j].get_color(), term_clock[i][j])?;
+            stdout_all += &format!("\x1b[0;{}m{}\x1b[0m", term_clock[i][j].get_color(), term_clock[i][j]);
         }
-        write!(stdout, "\n")?;
+        // write!(stdout, "\n")?;
+        stdout_all += "\n";
     }
+
+    // TODO SOLUTION IS TO TRY CROSS TERM INSTEAD TO SOLVE FLICKERING ISSUE 
+
+    // stdout.execute(cursor::MoveUp(24))?;
+    stdout.execute(cursor::MoveUp(13))?;
+    stdout.execute(terminal::Clear(terminal::ClearType::FromCursorDown))?;
+    writeln!(stdout, "{}", stdout_all)?;
 
     Ok(())
 }
@@ -46,7 +47,7 @@ fn main() -> Result<()> {
 
     // Check time endlessly
     loop {
-        // Update time every second 
+        // Update time every second
         sleep(Duration::from_millis(1000));
 
         // change and update display
@@ -55,6 +56,5 @@ fn main() -> Result<()> {
         update_exam_time(&mut term_clock, &mut exam);
 
         display(&term_clock, &mut stdout)?;
-        
     }
 }
