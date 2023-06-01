0a8b0a98dac79b55333b78c6cc220372aa39ab17
Gitalexzhong <66319418+Gitalexzhong@users.noreply.github.com>
2023-06-01 22:02:37 +1000

    Fixed non consistant updates

--- src/main.rs
+++ src/main.rs
@@ -2,34 +2,53 @@ use std::{ thread::sleep, time::Duration, io::Stdout };
 use chrono::{ Local };
 use color_char::Character;
 
+use std::io::{stdout, Write};
+use crossterm::{
+    ExecutableCommand, QueueableCommand,
+    terminal, cursor, style::{self, Stylize, Print}, Result, queue
+};
+use crossterm::{execute, terminal::{EnterAlternateScreen, LeaveAlternateScreen}};
+
 mod clock;
 use clock::update_time;
 
 mod exam_timer;
 use exam_timer::{ ExamStatus, update_exam_time };
 
-use std::io::{ stdout, Write };
-use crossterm::{ cursor, terminal, Result, ExecutableCommand };
-
-fn display(term_clock: &[[Character; 80]; 24], stdout: &mut Stdout) -> Result<()> {
+fn display(term_clock: &[[Character; 80]; 24], stdout: &mut Stdout, force: bool) -> Result<()> {
     let mut stdout_all = "".to_string();
-    // for i in 0..24 {
-    for i in 0..12 {
-        for j in 0..40 {
-        // for j in 0..80 {
+    for i in 0..24 {
+    // for i in 0..12 {
+        // for j in 0..40 {
+        for j in 0..80 {
             // write!(stdout, "\x1b[0;{}m{}\x1b[0m", term_clock[i][j].get_color(), term_clock[i][j])?;
-            stdout_all += &format!("\x1b[0;{}m{}\x1b[0m", term_clock[i][j].get_color(), term_clock[i][j]);
+            // stdout_all += &format!("\x1b[0;{}m{}\x1b[0m", term_clock[i][j].get_color(), term_clock[i][j]);
+            if term_clock[i][j] != Character::default() || force {
+                queue!(stdout, cursor::MoveTo(j as u16, i as u16), Print(format!("\x1b[0;{}m{}\x1b[0m", term_clock[i][j].get_color(), term_clock[i][j])))?;
+            }
+
         }
         // write!(stdout, "\n")?;
-        stdout_all += "\n";
+        // stdout_all += "\n";
     }
 
     // TODO SOLUTION IS TO TRY CROSS TERM INSTEAD TO SOLVE FLICKERING ISSUE 
 
     // stdout.execute(cursor::MoveUp(24))?;
-    stdout.execute(cursor::MoveUp(13))?;
-    stdout.execute(terminal::Clear(terminal::ClearType::FromCursorDown))?;
-    writeln!(stdout, "{}", stdout_all)?;
+    // stdout.execute(cursor::MoveUp(13))?;
+    // stdout.execute(terminal::Clear(terminal::ClearType::FromCursorDown))?;
+    // writeln!(stdout, "{}", stdout_all)?;
+    stdout.flush()?;
+
+
+    Ok(())
+}
+
+fn init_display(term_clock: &mut [[Character; 80]; 24], stdout: &mut Stdout, exam: &mut ExamStatus) -> Result<()> {
+    
+    update_time(term_clock);
+    update_exam_time(term_clock, exam);
+    display(&term_clock, stdout, true)?;
 
     Ok(())
 }
@@ -42,9 +61,7 @@ fn main() -> Result<()> {
 
     // Initial display
     let mut term_clock = [[Character::default(); 80]; 24];
-    update_time(&mut term_clock);
-    update_exam_time(&mut term_clock, &mut exam);
-    display(&term_clock, &mut stdout)?;
+    init_display(&mut term_clock, &mut stdout, &mut exam)?;
 
     // Check time endlessly
     loop {
@@ -56,6 +73,7 @@ fn main() -> Result<()> {
         update_time(&mut term_clock);
         update_exam_time(&mut term_clock, &mut exam);
 
-        display(&term_clock, &mut stdout)?;
+        display(&term_clock, &mut stdout, false)?;
     }
 }
+
